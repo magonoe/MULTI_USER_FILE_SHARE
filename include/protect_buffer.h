a92da9f6fe530967451d5c9deb4bd1a9f336e896
magonoe <ccouturier@et.esiea.fr>
2018-03-08 23:14:20 +0100

    FINIS ENFIN gst

--- include/protect_buffer.h
+++ include/protect_buffer.h
@@ -1,63 +1,78 @@
 #ifndef _PROTECT_BUFFER_H
 #define _PROTECT_BUFFER_H
 
-int gen_key	(unsigned char *key,		int key_length);
+int gen_key	(		unsigned char *key,			int key_length
+					);
 
-int genKc(unsigned char **Kc);
-int genIV(unsigned char **IV);
-int loadInput(unsigned char **output, 	unsigned int *output_len,
-				const char *filename, int padding);
+int genKc(			unsigned char **Kc
+					);
+
+int genIV(			unsigned char **IV
+					);
+
+int loadInput(		unsigned char **output, 	unsigned int *output_len,
+					const char *filename, 		int *paddinge
+					);
 
 int chiffre_buffer( unsigned char **output, 	unsigned int *output_len,
 					unsigned char *input, 		unsigned int input_len,
 					unsigned char *Kc,
 					unsigned char *IV
 					);
-int isDest(unsigned char * where,char *key);
-int chiffreKc( 	unsigned char **output, 	unsigned int *output_len,
-				unsigned char *Kc , const char *filename);
+
+int dechiffre_buffer( unsigned char **output, 	unsigned int *output_len,
+					unsigned char *input, 		unsigned int input_len,
+					unsigned char *Kc,
+					unsigned char *IV
+					);
+
+int isDest(			unsigned char * where,		char *key
+					);
+
+int chiffreKc( 		unsigned char **output, 	unsigned int *output_len,
+					unsigned char *Kc , 		const char *filename
+					);
 
 int dechiffreKc( 	unsigned char **output, 	unsigned int *output_len,
 					unsigned char *input, 		unsigned int input_len,
-					const char *filename);
+					const char *filename
+					);
+
 int findKc( 		unsigned char *input, 		unsigned int input_len, 
 					char *keypub,				char *keypriv,
 					unsigned char **Kc,			unsigned int *Kc_len, 
-					unsigned int *offset);
-int signFic(unsigned char **output, 	unsigned int *output_len,
-			const char *filename, char *key);
+					unsigned int *offset
+					);
 
+int signFic(		unsigned char **output, 	unsigned int *output_len,
+					const char *filename, 		char *key
+					);
 
-int signeKpub( 	unsigned char **output, 	unsigned int *output_len,
-				const char *filename);
+int remove_padding(	unsigned int * output_len,
+					unsigned char *input, 		unsigned int input_len ,
+					char pad
+					);
 
-int verifySign(unsigned char *input, unsigned int input_len,
-								char *key);
+int loadIv(			unsigned char **output, 	unsigned char *input
+					);
 
-int checkArg(int argc, char ** argv);
+int signeKpub( 		unsigned char **output, 	unsigned int *output_len,
+					const char *filename
+					);
 
-int encrypt(int argc, char **argv);
-int decrypt(int argc, char **argv);
+int verifySign(		unsigned char *input, 		unsigned int input_len,
+					char *key
+					);
 
+int checkArg(		int argc, 					char ** argv
+					);
 
+int encrypt(		int argc,	 				char **argv
+					);
 
+int decrypt(		int argc, 					char **argv
+					);
 
-
-
-int protect_buffer(unsigned char **output, int *output_len,
-					unsigned char *input, int input_len,
-					unsigned char *master_key, int key_len,
-					unsigned char *salt, int salt_len);
-
-int unprotect_buffer(unsigned char **output, int *output_len,
-                      unsigned char *input, int input_len,
-                      unsigned char *master_key, int key_len,
-                      int salt_len);
-
-
-#define BAD_SIGN 0x500
-
-
-
+void help();
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,30 +3,22 @@
 #include <string.h>
 #include "protect_buffer.h"
 
-int main (int argc, char ** argv){
+int main (int argc, char ** argv)
+{
+	int choix =0;
+	choix = checkArg(argc, argv);
+	if (choix ==1)
+	{
+		encrypt(argc,argv);
+	}
 
-
-
-
-
-								//-e <input_file> <output_file> <my_sign_priv.pem> <my_ciph_pub.pem> [user1_ciph_pub.pem ... [userN_ciph_pub.pem]]
-								//-d <input_file> <output_file> <my_priv_ciph.pem> <my_pub_ciph.pem> <sender_sign_pub.pem>
-
-
-								int choix =0;
-								choix = checkArg(argc, argv);
-								printf("choix : %d\n",choix );
-
-
-								if (choix ==1)
-								{
-																encrypt(argc,argv);
-								}
-
-								if (choix ==2)
-								{
-																decrypt(argc,argv);
-								}
-
-								return 0;
+	else if (choix ==2)
+	{
+		decrypt(argc,argv);
+	}
+	else 
+	{
+		help();
+	}
+	return 0;
 }
